4974c14ef4a73688d4d98ac26bee319d67791a3e
unknown <julen.leremboure@laposte.net>
2022-01-21 21:54:03 +0100

    Menu added

---
+++ Assets/Menu/Scripts/MainMenuUI.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using TMPro;
+
+public class MainMenuUI : MonoBehaviour
+{
+    public GameObject MainMenuPanel;
+    public GameObject CreditsPanel;
+
+    private void Start()
+    {
+        MainMenuPanel.SetActive(true);
+        CreditsPanel.SetActive(false);
+    }
+
+    public void PlayGame()
+    {
+        PlayButtonSound();
+        //SceneManager.LoadScene("IntroDialogue");
+    }
+
+
+    public void Credits()
+    {
+        PlayButtonSound();
+        MainMenuPanel.SetActive(false);
+        CreditsPanel.SetActive(true);
+    }
+
+    public void QuitGame()
+    {
+        PlayButtonSound();
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#else
+        Application.Quit();
+#endif
+    }
+
+    public void BackToMenu()
+    {
+        PlayButtonSound();
+        CreditsPanel.SetActive(false);
+        MainMenuPanel.SetActive(true);
+    }
+
+    private void PlayButtonSound()
+    {
+        //AudioManager.instance.PlayOneShot("UI_button");
+    }
+}
---
+++ Assets/Menu/Scripts/PauseUI.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using TMPro;
+
+public class PauseUI : MonoBehaviour
+{
+    [SerializeField] private GameObject pauseMenuCanvas;
+
+    private bool isPaused;
+
+    private void Start()
+    {
+        TogglePauseUI(false);
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (!isPaused)
+            {
+                TogglePauseUI(true);
+            }
+            else
+            {
+                BackToTheGame();
+            }
+        }
+    }
+
+    public void Continuer()
+    {
+        PlayButtonSound();
+        BackToTheGame();
+    }
+
+    public void ChargerMenu()
+    {
+        PlayButtonSound();
+
+        //SceneManager.LoadScene("Menu");
+    }
+
+    private void BackToTheGame()
+    {
+        PlayButtonSound();
+
+        TogglePauseUI(false);
+    }
+
+    private void PlayButtonSound()
+    {
+        //AudioManager.instance.PlayOneShot("UI_button");
+    }
+
+    private void TogglePauseUI(bool isActive)
+    {
+        isPaused = isActive;
+        pauseMenuCanvas.SetActive(isActive);
+
+        Time.timeScale = isActive ? 0 : 1;
+    }
+}
