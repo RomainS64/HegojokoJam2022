1bf3d308880fc53ff36aa941b67f22d883ed28e1
Mortox-9 <76782611+Mortox-9@users.noreply.github.com>
2022-01-22 00:54:07 -0500

    flipixe et flipigrec

--- Assets/Scrpts/PlayerMouvements.cs
+++ Assets/Scrpts/PlayerMouvements.cs
@@ -7,10 +7,30 @@ public class PlayerMouvements : MonoBehaviour
     [SerializeField] private float speedX;
     [SerializeField] private float speedY;
     [SerializeField] private Transform topY, downY;
+
+    private Camera camera;
     private Rigidbody2D rb;
+    private SpriteRenderer sprite;
     private void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        camera = FindObjectOfType<Camera>();
+        sprite = GetComponent<SpriteRenderer>();
+    }
+    private void Update()
+    {
+
+
+        Vector2 Point_1 = camera.ScreenToWorldPoint(Input.mousePosition);
+
+        if (Point_1.x > transform.position.x)
+        {
+            sprite.flipX = false;
+        }
+        else
+        {
+            sprite.flipX = true;
+        }
     }
     private void FixedUpdate()
     {
