765f6729a6a162edbb55a9072359f6865f5a8d92
Miguel Viegas <miguel.viegas64600@gmail.com>
2022-01-22 00:47:53 -0500

    Health Bar 2/2

--- Assets/Scrpts/Health.cs
+++ Assets/Scrpts/Health.cs
@@ -1,19 +1,22 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Health : MonoBehaviour
 {
 
-    public int curHealth = 0;
-    public int maxHealth = 100;
+    public float curHealth;
+    public float maxHealth;
 
-    public HealthBar healthBar;
+    public Slider healthBar;
 
     // Start is called before the first frame update
     void Start()
     {
         curHealth = maxHealth;
+        healthBar.value = curHealth;
+        healthBar.maxValue = maxHealth;
     }
 
     // Update is called once per frame
@@ -29,6 +32,6 @@ public class Health : MonoBehaviour
     {
         curHealth -= damage;
 
-        healthBar.SetHealth(curHealth);
+        healthBar.value = curHealth;
     }
 }
