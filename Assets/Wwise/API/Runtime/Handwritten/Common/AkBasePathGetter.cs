4e069baa442e28014dc5fb6df0066cd33fcddcf1
Camille Estanguet <estanguetcamille@gmail.com>
2022-01-22 15:35:56 +0100

    First SB added

--- Assets/Wwise/API/Runtime/Handwritten/Common/AkBasePathGetter.cs
+++ Assets/Wwise/API/Runtime/Handwritten/Common/AkBasePathGetter.cs
@@ -150,7 +150,7 @@ public partial class AkBasePathGetter
 
 		try
 		{
-			if (System.IO.Path.GetPathRoot(SoundBankDest) == "")
+			if (SoundBankDest != "" && System.IO.Path.GetPathRoot(SoundBankDest) == "")
 			{
 				// Path is relative, make it full
 				SoundBankDest = AkUtilities.GetFullPath(System.IO.Path.GetDirectoryName(WwiseProjectFullPath), SoundBankDest);
