63c526b6f4c78a62283deb7d27a63789279c55d6
Miguel Viegas <miguel.viegas64600@gmail.com>
2022-01-22 00:30:24 -0500

    HealthBar 1/2

---
+++ Assets/Scrpts/HealthBar.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HealthBar : MonoBehaviour
+{
+
+    public Slider healthBar;
+    public Health playerHealth;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerHealth = GameObject.FindGameObjectWithTag("Player").GetComponent<Health>();
+        healthBar = GetComponent<Slider>();
+        healthBar.maxValue = playerHealth.maxHealth;
+        healthBar.value = playerHealth.maxHealth;
+    }
+
+    public void SetHealth(int hp)
+    {
+        healthBar.value = hp;
+    }
+}
