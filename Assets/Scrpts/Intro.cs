f4f01222c85ad3803ed0b9f8d5810f273db91623
RomainS64 <salha.romain64@gmail.com>
2022-01-23 12:31:17 -0500

    cq

--- Assets/Scrpts/Intro.cs
+++ Assets/Scrpts/Intro.cs
@@ -33,6 +33,6 @@ public class Intro : MonoBehaviour
         fadeCanvas.SetActive(false);
         InGameCanvas.SetActive(true);
         waveSpawner.StartFirstWave();
-
+        AkSoundEngine.PostEvent("playMusicGog", gameObject);
     }
 }
