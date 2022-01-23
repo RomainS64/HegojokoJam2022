20060ddde7b2e0962f6403e34245062babafb00f
RomainS64 <salha.romain64@gmail.com>
2022-01-22 23:42:36 -0500

    Fin Scene de fin

--- Assets/Scrpts/EndScene.cs
+++ Assets/Scrpts/EndScene.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class EndScene : MonoBehaviour
 {
@@ -9,15 +10,33 @@ public class EndScene : MonoBehaviour
 
     [SerializeField] private float scrollTime;
     [SerializeField] private GameObject deathBodyPrefab;
-
+    [SerializeField] private Text T2, T3;
     Camera camera;
     IEnumerator scroller;
 
     private void Start()
     {
         camera = FindObjectOfType<Camera>();
+        
+        
         int nbKills = PlayerPrefs.GetInt("nbKill", 0);
-        for(int i = 1; i <= nbKills; i++)
+
+        int coca = PlayerPrefs.GetInt("coca", 0);
+        if (coca > 0)
+        {
+            T2.text = "You snorted " +coca+"00g of Big C.";
+            T3.text = "You killed " + nbKills + " innocents.";
+        }
+        else
+        {
+            T2.text = "Hopefully, you didn't get addict to Big C.";
+            T3.text = "However, you killed " + nbKills + " innocents.";
+        }
+        
+        
+
+
+        for (int i = 1; i <= nbKills; i++)
         {
             GameObject newBody = Instantiate(deathBodyPrefab);
             Debug.Log("Instantiate " + i + ":" + "(" + PlayerPrefs.GetFloat("posX" + i, 0) + "," + PlayerPrefs.GetFloat("posY" + i, 0));
